7a185e334c5b888d50259b3dfa1c083ee5b97219
Tushar Vaghela <tushar.v@proximity.tech>
2021-12-05 00:57:44 +0530

    feature: rock paper scissors game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import { Title } from './components/Title';
+import { Round } from './components/Round';
+import { Playground } from './components/Playground';
+import { Profile } from './components/Profile';
+import { User } from './components/User';
+import { Choice } from './components/Choice';
+import { Computer } from './components/Computer';
+import { Score } from './components/Score';
+import { Message } from './components/Message';
+import { Reset } from './components/Reset';
+
+import { settings } from './configs/game';
+
+import rock from './assets/rock.png';
+import paper from './assets/paper.png';
+import scissors from './assets/scissors.png';
+import trophy from './assets/trophy.png';
+
+import './styles.css';
+
+export default function App() {
+  let [game, setGame] = useState({
+    userSelection: '',
+    pcSelection: '',
+    round: 0,
+    userScore: 0,
+    pcScore: 0,
+    message: '',
+  });
+
+  const reset = () => {
+    setGame({
+      ...game,
+      userSelection: '',
+      pcSelection: '',
+      round: 0,
+      userScore: 0,
+      pcScore: 0,
+      message: '',
+    });
+  };
+
+  const { winMessage, tieMessage, lostMessage, winTarget } = settings;
+  const { pcScore, userScore } = game;
+
+  const play = (e) => {
+    if (pcScore < winTarget) {
+      const userSelection = e.target.parentNode.getAttribute('value');
+      const pcSelection = ['Rock', 'Paper', 'Scissors'][
+        Math.floor(Math.random() * 3)
+      ];
+
+      userSelection === pcSelection
+        ? setGame({
+            ...(game.message = tieMessage),
+          })
+        : (userSelection === 'Rock' && pcSelection === 'Scissors') ||
+          (userSelection === 'Paper' && pcSelection === 'Rock') ||
+          (userSelection === 'Scissors' && pcSelection === 'Paper')
+        ? setGame({
+            ...(game.userScore += 1),
+            ...(game.message = winMessage),
+          })
+        : setGame({
+            ...(game.pcScore += 1),
+            ...(game.message = lostMessage),
+          });
+
+      setGame({
+        ...game,
+        round: (game.round += 1),
+        userSelection,
+        pcSelection,
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Title />
+      <Round {...game} />
+      <Playground>
+        <Profile>
+          <User {...game} trophyIcon={trophy}>
+            <Choice {...game} value='Rock' onClick={play} choiceIcon={rock} />
+            <Choice {...game} value='Paper' onClick={play} choiceIcon={paper} />
+            <Choice
+              {...game}
+              value='Scissors'
+              onClick={play}
+              choiceIcon={scissors}
+            />
+          </User>
+          <Score score={userScore} />
+        </Profile>
+        <Message {...game} />
+        <Profile>
+          <Computer
+            {...game}
+            rockIcon={rock}
+            paperIcon={paper}
+            scissorsIcon={scissors}
+            trophyIcon={trophy}
+          />
+          <Score score={pcScore} />
+        </Profile>
+      </Playground>
+      <Reset {...game} onClick={reset} />
     </div>
   );
 }
-
-export default App;
